8fdf39b8337177cca1106f3dac3df3c560655ced
Primoz <primoz969@gmail.com>
2018-06-02 23:13:26 +0200

    adds playing songs functionality

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+				</div> <!-- End mainContent -->
+			</div> <!-- End mainViewContainer -->
+		</div> <!-- End topContainer -->	
+		<?php include("includes/nowPlayingBarContainer.php") ?>
+	</div> <!-- End mainContainer -->	
+</body>
+
+</html>
---
+++ includes/navBarContainer.php
@@ -0,0 +1,26 @@
+<div id="navBarContainer">
+	<nav class="navbar">
+		<a href="index.php" class="logo">
+			<img src="assets/images/icons/logo2.png">
+		</a>
+
+		<div class="group">
+			<div class="navItem">
+				<a href="search.php" class="navItemLink">Search 
+					<img src="assets/images/icons/search.png" class="icon" alt="Search">
+				</a>
+			</div><!-- End navItem 1 -->
+		</div><!-- End group  1 -->
+		<div class="group">
+			<div class="navItem">
+				<a href="browse.php" class="navItemLink">Browse</a>
+			</div><!-- End navItem 2 -->
+			<div class="navItem">
+				<a href="yourMusic.php" class="navItemLink">Your Music</a>
+			</div><!-- End navItem 3 -->
+			<div class="navItem">
+				<a href="profile.php" class="navItemLink">Primoz Babsek</a>
+			</div><!-- End navItem 4 -->
+		</div><!-- End group 2 -->
+	</nav>
+</div> <!-- End navBarContainer -->
